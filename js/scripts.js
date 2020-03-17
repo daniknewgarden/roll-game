df83805c42d160069e203336b65c025736c53c31
Danik <daniknewgarden@gmail.com>
2020-03-17 21:45:38 +0200

    Aded scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,42 @@
 let body = document.body,
     startScreen = document.querySelector('.before-screen'),
     gameContent = document.querySelector('.game-screen'),
-    beforeScreen  = document.querySelector('game-screen'),
+    afterScreen  = document.querySelector('after-screen'),
+    timer = document.querySelector('#timer'),
+    reelText = document.querySelector('.start-text'),
     startBtn = document.querySelector('#start'),
+    rollItems = document.querySelectorAll('.rolling-items__item'),
+    rollStyle = document.getElementById('roll').style,
+    rollBackground = document.querySelector('.roll__background');
+
+    function showGame () {
+        startScreen.classList.remove('show');
+        gameContent.classList.add('show');
+    }
+
+    startBtn.addEventListener('click', function () {
+        console.log('ok');
+        showGame(); 
+    });
+
+    function startTimer(num) {
+        num = 30;
+        let timeInterval = setInterval(updateTime, 1000);
+
+        function updateTime(params) {
+            num--;
+            timer.textContent = num;
+
+            if (num < 1) {
+                clearInterval(timeInterval);
+                console.log('ok');
+            }
+        }
+
+    }
+
+    function startGame() {
+        reelTitle.classList.add('hide');
+    }
+
+    
